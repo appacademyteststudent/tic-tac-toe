ba5242fada9de522fc203da6ab8d81e197d6ba05
dion <dionbarrett1234@gmail.com>
2016-11-17 10:18:36 -0800

    k

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,90 +1,85 @@
 var namespace = "http://www.w3.org/2000/svg"
 
-var circle = document.createElementNS(namespace, "circle")
-circle.setAttribute("cx", 100)
-circle.setAttribute("cy", 100)
-circle.setAttribute("r", 30)
-circle.setAttribute("fill","red")
-var canvas = document.getElementById("game-board")
-canvas.appendChild(circle)
+
+function makeshape1(){
+  var circle =document.createElementNS(namespace,"circle")
+  circle.setAttribute("cx",55)
+  circle.setAttribute("cy",45)
+  circle.setAttribute("r",45)
+  circle.setAttribute("fill","blue")
+  var canvas = document.getElementById("game-board")
+  canvas.appendChild(circle)
+}
+function makeshape2(){
+  var circle =document.createElementNS(namespace,"circle")
+  circle.setAttribute("cx",155)
+  circle.setAttribute("cy",45)
+  circle.setAttribute("r",45)
+  circle.setAttribute("fill","green")
+  var canvas = document.getElementById("game-board")
+  canvas.appendChild(circle)
+}
+function makeshape3(){
+  var circle =document.createElementNS(namespace,"circle")
+  circle.setAttribute("cx",255)
+  circle.setAttribute("cy",45)
+  circle.setAttribute("r",45)
+  circle.setAttribute("fill","blue")
+  var canvas = document.getElementById("game-board")
+  canvas.appendChild(circle)
+}
+function makeshape4(){
+  var circle =document.createElementNS(namespace,"circle")
+  circle.setAttribute("cx",55)
+  circle.setAttribute("cy",145)
+  circle.setAttribute("r",45)
+  circle.setAttribute("fill","green")
+  var canvas = document.getElementById("game-board")
+  canvas.appendChild(circle)
+}
+function makeshape5(){
+  var circle =document.createElementNS(namespace,"circle")
+  circle.setAttribute("cx",155)
+  circle.setAttribute("cy",145)
+  circle.setAttribute("r",45)
+  circle.setAttribute("fill","blue")
+  var canvas = document.getElementById("game-board")
+  canvas.appendChild(circle)
+}  var circle =document.createElementNS(namespace,"circle")
+function makeshape6(){
+  var circle =document.createElementNS(namespace,"circle")
+  circle.setAttribute("cx",255)
+  circle.setAttribute("cy",145)
+  circle.setAttribute("r",45)
+  circle.setAttribute("fill","green")
+  var canvas = document.getElementById("game-board")
+  canvas.appendChild(circle)
+}
+function makeshape7(){
+  var circle =document.createElementNS(namespace,"circle")
+  circle.setAttribute("cx",55)
+  circle.setAttribute("cy",245)
+  circle.setAttribute("r",45)
+  circle.setAttribute("fill","blue")
+  var canvas = document.getElementById("game-board")
+  canvas.appendChild(circle)
+}
+function makeshape8(){
+  var circle =document.createElementNS(namespace,"circle")
+  circle.setAttribute("cx",155)
+  circle.setAttribute("cy",245)
+  circle.setAttribute("r",45)
+  circle.setAttribute("fill","green")
+  var canvas = document.getElementById("game-board")
+  canvas.appendChild(circle)
 }
 
-function makeShape2() {
-var circle = document.createElementNS(namespace, "circle")
-circle.setAttribute("cx", 210)
-circle.setAttribute("cy", 100)
-circle.setAttribute("r", 30)
-circle.setAttribute("fill","red")
-var canvas = document.getElementById("game-board")
-canvas.appendChild(circle)
-}
-
-function makeShape5() {
-var circle = document.createElementNS(namespace, "circle")
-circle.setAttribute("cx", 210)
-circle.setAttribute("cy", 210)
-circle.setAttribute("r", 30)
-circle.setAttribute("fill","red")
-var canvas = document.getElementById("game-board")
-canvas.appendChild(circle)
-}
-
-function makeShape9() {
-var circle = document.createElementNS(namespace, "circle")
-circle.setAttribute("cx", 320)
-circle.setAttribute("cy", 320)
-circle.setAttribute("r", 30)
-circle.setAttribute("fill","red")
-var canvas = document.getElementById("game-board")
-canvas.appendChild(circle)
-}
-
-function makeShape3() {
-var circle = document.createElementNS(namespace, "circle")
-circle.setAttribute("cx", 320)
-circle.setAttribute("cy", 100)
-circle.setAttribute("r", 30)
-circle.setAttribute("fill","blue")
-var canvas = document.getElementById("game-board")
-canvas.appendChild(circle)
-}
-
-function makeShape4() {
-var circle = document.createElementNS(namespace, "circle")
-circle.setAttribute("cx", 100)
-circle.setAttribute("cy", 210)
-circle.setAttribute("r", 30)
-circle.setAttribute("fill","blue")
-var canvas = document.getElementById("game-board")
-canvas.appendChild(circle)
-}
-
-function makeShape6() {
-var circle = document.createElementNS(namespace, "circle")
-circle.setAttribute("cx", 320)
-circle.setAttribute("cy", 210)
-circle.setAttribute("r", 30)
-circle.setAttribute("fill","blue")
-var canvas = document.getElementById("game-board")
-canvas.appendChild(circle)
-}
-
-function makeShape7() {
-var circle = document.createElementNS(namespace, "circle")
-circle.setAttribute("cx", 100)
-circle.setAttribute("cy", 320)
-circle.setAttribute("r", 30)
-circle.setAttribute("fill","blue")
-var canvas = document.getElementById("game-board")
-canvas.appendChild(circle)
-}
-
-function makeShape8() {
-var circle = document.createElementNS(namespace, "circle")
-circle.setAttribute("cx", 210)
-circle.setAttribute("cy", 320)
-circle.setAttribute("r", 30)
-circle.setAttribute("fill","blue")
-var canvas = document.getElementById("game-board")
-canvas.appendChild(circle)
+function makeshape9(){
+  var circle =document.createElementNS(namespace,"circle")
+  circle.setAttribute("cx",255)
+  circle.setAttribute("cy",245)
+  circle.setAttribute("r",45)
+  circle.setAttribute("fill","blue")
+  var canvas = document.getElementById("game-board")
+  canvas.appendChild(circle)
 }
